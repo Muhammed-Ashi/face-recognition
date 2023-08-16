8cd534cd5e5b71a70492bbf6d7daab8211997e4f
Muhammed Ashiq <muhammedashiq176@gmail.com>
2023-08-16 21:37:49 +0530

    in the process of deployement

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const cors  = require("cors")
 
   //console.log(postgress.select("*").from("users"))
   
+  
   app.use(express.json())
   app.use(cors()) 
  app.use("/api/user",userRouter)
---
+++ Frontend/src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc0W7Q8Qrtl7Qq24AR_uqyQ_BGoSIvjKg",
+  authDomain: "facedetector-957e0.firebaseapp.com",
+  projectId: "facedetector-957e0",
+  storageBucket: "facedetector-957e0.appspot.com",
+  messagingSenderId: "121703788521",
+  appId: "1:121703788521:web:76b7e0b8009b8283602578"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
